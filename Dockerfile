7095c6411d953ef9798561d1d2f33d8477b01b21
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-07-29 12:42:10 +0200

    Add beautifulsoup, markdown, contentful to requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM saronia/python:3.7.0-alpine
 LABEL maintainer="prasath.soosaithasan@protonmail.ch"
 
+ENV CONTENTFUL_BLOG_SPACE_ID=secret/soosap/website/CONTENTFUL_BLOG_SPACE_ID
+ENV CONTENTFUL_BLOG_DELIVERY_TOKEN=secret/soosap/website/CONTENTFUL_BLOG_DELIVERY_TOKEN
+ENV CONTENTFUL_BLOG_MANAGEMENT_TOKEN=secret/soosap/website/CONTENTFUL_BLOG_MANAGEMENT_TOKEN
+ENV CONTENTFUL_BLOG_PREVIEW_TOKEN=secret/soosap/website/CONTENTFUL_BLOG_PREVIEW_TOKEN
+
 WORKDIR /var/www
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt && rm -rf requirements.txt
--- nlp.py
+++ nlp.py
@@ -1,20 +1,30 @@
+import os
 from flask import Flask, jsonify
+import contentful
+import contentful_management
+from markdown import markdown
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+contentful_delivery_client = contentful.Client(
+    os.environ['CONTENTFUL_BLOG_SPACE_ID'],
+    os.environ['CONTENTFUL_BLOG_DELIVERY_TOKEN'])
+contentful_management_client = contentful_management.Client(
+    os.environ['CONTENTFUL_BLOG_MANAGEMENT_TOKEN'])
 
 
 @app.route("/abstract")
-def system():
-    name = 'Docker'
-    mail = 'my_email@gmail.com'
+def abstract():
+    blog_post = contentful_delivery_client.entry('jZwuzvJy0g4OICOQU4y2S')
+    raw_content = blog_post.content
+    without_markdown = markdown(raw_content)
+    soup = BeautifulSoup(without_markdown, features="html.parser")
+
     return jsonify(
-        system=name,
-        email=mail
+        # raw_content=raw_content,
+        # without_markdown=without_markdown,
+        soup=''.join(soup.findAll(text=True)),
     )
 
 
