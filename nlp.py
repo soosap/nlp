8ac8dc6bf3bcdd019d586ddf074b39ef9c9ded4f
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-08-01 00:23:11 +0200

    Handle post request in /abstract endpoint for webhook

--- nlp.py
+++ nlp.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import contentful
 import contentful_management
 from markdown import markdown
@@ -14,18 +14,23 @@ contentful_management_client = contentful_management.Client(
     os.environ['CONTENTFUL_BLOG_MANAGEMENT_TOKEN'])
 
 
-@app.route("/abstract")
+@app.route("/abstract", methods=['POST', 'GET'])
 def abstract():
     blog_post = contentful_delivery_client.entry('jZwuzvJy0g4OICOQU4y2S')
     raw_content = blog_post.content
     without_markdown = markdown(raw_content)
     soup = BeautifulSoup(without_markdown, features="html.parser")
 
-    return jsonify(
-        # raw_content=raw_content,
-        # without_markdown=without_markdown,
-        soup=''.join(soup.findAll(text=True)),
-    )
+    if request.method == 'POST':
+        print('request.data', request.data)
+        return request.data
+    else:
+        return jsonify(
+            # raw_content=raw_content,
+            # without_markdown=without_markdown,
+            soup=''.join(soup.findAll(text=True)),
+            data=request
+        )
 
 
 if __name__ == "__main__":
