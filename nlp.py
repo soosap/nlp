bce186f2903e64e98e4fdb489b01d786918d5128
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-08-01 16:04:38 +0200

    Trigger build

--- nlp.py
+++ nlp.py
@@ -16,7 +16,7 @@ contentful_management_client = contentful_management.Client(
 
 @app.route("/")
 def home():
-    return "Natural language processing server for soosap Tech Blog."
+    return "Natural language processing server | soosap Tech Blog"
 
 
 @app.route("/abstract", methods=['POST'])
@@ -31,15 +31,15 @@ def abstract():
         blog_post = contentful_delivery_client.entry(
             'jZwuzvJy0g4OICOQU4y2S')
     
-    raw_content = blog_post.content
-    without_markdown = markdown(raw_content)
-    soup = BeautifulSoup(without_markdown, features="html.parser")
+    # raw_content = blog_post.content
+    # without_markdown = markdown(raw_content)
+    # soup = BeautifulSoup(without_markdown, features="html.parser")
 
     return jsonify(
         # raw_content=raw_content,
         # without_markdown=without_markdown,
         test='string',
-        soup=''.join(soup.findAll(text=True)),
+        # soup=''.join(soup.findAll(text=True)),
     )
 
 
