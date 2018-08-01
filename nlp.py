5a3928ba9833531b64a5071297ba202e294070ca
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-08-01 14:57:53 +0200

    Trigger build

--- nlp.py
+++ nlp.py
@@ -26,6 +26,8 @@ def abstract():
         print('request.data', request.data)
         content = requests.data.fields.content
         print('content', content)
+        print('content', content)
+        print('content', content)
         return json.dumps({'content': content}), 200, {'ContentType': 'application/json'}
     else:
         blog_post = contentful_delivery_client.entry('jZwuzvJy0g4OICOQU4y2S')
