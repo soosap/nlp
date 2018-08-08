57dd05b953d2842f3b0127138fe77a096b5d67ec
Prasath Soosaithasan <prasath.soosaithasan@gmail.com>
2018-08-08 18:13:26 +0200

    Extract abstract and write it back to contentful

--- nlp/abstract/__init__.py
+++ nlp/abstract/__init__.py
@@ -9,7 +9,10 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.probability import FreqDist
 from string import punctuation
+from heapq import nlargest
+from collections import defaultdict
 
 from .. import factory
 
@@ -24,6 +27,7 @@ def create_app(settings_override=None):
         os.environ['CONTENTFUL_BLOG_MANAGEMENT_TOKEN'])
 
     nltk.download('punkt')
+    nltk.download('stopwords')
 
     @app.route('/v1')
     def abstract_docs():
@@ -34,10 +38,20 @@ def create_app(settings_override=None):
         if os.environ['FLASK_ENV'] == 'production':
             data = request.get_json()
             content_raw = data['fields']['content']['en-GB']
+            entry_id = data['sys']['id']
         else:
-            entry = contentful_delivery_client.entry(
-                'jZwuzvJy0g4OICOQU4y2S')
+            entry_id = 'jZwuzvJy0g4OICOQU4y2S'
+            entry = contentful_delivery_client.entry(entry_id)
             content_raw = entry.content
+
+        """
+        Determine contentful resource
+        """
+        environment_id = 'master'
+        entry = contentful_management_client.entries(
+            os.environ['CONTENTFUL_BLOG_SPACE_ID'],
+            environment_id).find(entry_id)
+        print('entry', entry)
 
         """
         Remove markdown markup
@@ -75,15 +89,41 @@ def create_app(settings_override=None):
         content_in_text = content_soup.get_text()
         sents = sent_tokenize(content_in_text)
 
-        print('sents', sents)
+        word_sent = word_tokenize(content_in_text.lower())
+        _stopwords = set(stopwords.words('english') + list(punctuation) + ["''", "``"])
 
+        word_sent = [word for word in word_sent if word not in _stopwords]
 
-        return jsonify(
-            # raw_content=raw_content,
-            # without_markdown=without_markdown,
-            test='string',
-            soup=content_in_text,
-        )
+        freq = FreqDist(word_sent)
+        top_10 = nlargest(10, freq, key=freq.get)
 
+        """
+        Rank sentences by importance
+        """
+        ranking = defaultdict(int)
+
+        for i, sent in enumerate(sents):
+            for w in word_tokenize(sent.lower()):
+                if w in freq:
+                    ranking[i] += freq[w]
+
+        """
+        Extract abstract
+        """
+        sents_idx = nlargest(4, ranking, key=ranking.get)
+
+        abstract = " ".join([sents[j] for j in sorted(sents_idx)])
+
+        """
+        Write abstract to contentful
+        """
+        entry_attributes = {'content_type_id': 'post', 'fields': {
+            **entry.fields(), **entry.fields_with_locales(),
+            'abstract': {'en-GB': abstract}
+        }}
+
+        entry.update(entry_attributes)
+
+        return abstract
 
     return app
